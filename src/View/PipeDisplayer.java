462cfbf3d079a5646387e74993a6aa4261060557
amitsaar <amitsaar@10.160.11.190>
2018-12-14 12:03:46 +0200

    Add PipeDisplayer

---
+++ src/View/PipeDisplayer.java
@@ -0,0 +1,18 @@
+package View;
+
+import javafx.beans.property.StringProperty;
+import javafx.scene.canvas.Canvas;
+
+public class PipeDisplayer extends Canvas{
+	
+	char[][] pipeData;
+	
+	//pics fileName
+	private StringProperty startPointFileName;
+	private StringProperty targetPointFileName;
+	private StringProperty winFileName;
+	private StringProperty curvedPipeFileName;
+	private StringProperty straightPipeFileName;
+	
+
+}
