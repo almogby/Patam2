8f864aa7c41aa24a744b4272f392bfd6603fb309
amitsaar <amitsaar@10.160.11.190>
2018-12-15 09:46:09 +0200

    Added open File, setters and getters of images

--- src/View/MainWindowController.java
+++ src/View/MainWindowController.java
@@ -1,5 +1,23 @@
 package View;
 
+import java.io.File;
+
+import javafx.stage.FileChooser;
+
 public class MainWindowController {
 	
+	public void start() {
+		System.out.println("Start");
+	}
+	
+	public void openFile() {
+		FileChooser fc= new FileChooser();
+		fc.setTitle("open maze file");
+		fc.setInitialDirectory(new File("./resources"));
+		//fc.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Only XML", "*.xml"));
+		File chosen = fc.showOpenDialog(null);
+		if (chosen != null) {
+			System.out.println(chosen.getName());
+		}
+	}
 }
--- src/View/PipeDisplayer.java
+++ src/View/PipeDisplayer.java
@@ -1,18 +1,118 @@
 package View;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 
 public class PipeDisplayer extends Canvas{
 	
 	char[][] pipeData;
+	int cRow, cCol;
 	
 	//pics fileName
+	private StringProperty backgroundFileName;
 	private StringProperty startPointFileName;
 	private StringProperty targetPointFileName;
 	private StringProperty winFileName;
 	private StringProperty curvedPipeFileName;
 	private StringProperty straightPipeFileName;
 	
+	
+	public PipeDisplayer() {
+		this.startPointFileName = new SimpleStringProperty();
+		this.targetPointFileName = new SimpleStringProperty();
+		this.winFileName = new SimpleStringProperty();
+		this.curvedPipeFileName = new SimpleStringProperty();
+		this.straightPipeFileName = new SimpleStringProperty();
+		cCol=0;
+		cRow=1;
+	}
+	
+	public void setPipeData (char[][] pipeData) {
+		this.pipeData=pipeData;
+	}
+
+	public String getBackgroundFileName() {
+		return backgroundFileName.get();
+	}
+
+
+	public void setBackgroundFileName(String backgroundFileName) {
+		this.backgroundFileName.set(backgroundFileName);
+	}
+	public String getStartPointFileName() {
+		return startPointFileName.get();
+	}
+
+
+	public void setStartPointFileName(String startPointFileName) {
+		this.startPointFileName.set(startPointFileName);
+	}
+
+
+	public String getTargetPointFileName() {
+		return targetPointFileName.get();
+	}
+
+
+	public void setTargetPointFileName(String targetPointFileName) {
+		this.targetPointFileName.set(targetPointFileName);
+	}
+
+
+	public String getWinFileName() {
+		return winFileName.get();
+	}
+
+
+	public void setWinFileName(String winFileName) {
+		this.winFileName.set(winFileName);
+	}
+
+
+	public String getCurvedPipeFileName() {
+		return curvedPipeFileName.get();
+	}
+
+
+	public void setCurvedPipeFileName(String curvedPipeFileName) {
+		this.curvedPipeFileName.set(curvedPipeFileName);
+	}
+
+
+	public String getStraightPipeFileName() {
+		return straightPipeFileName.get();
+	}
+
+
+	public void setStraightPipeFileName(String straightPipeFileName) {
+		this.straightPipeFileName.set(straightPipeFileName);
+	}
+	
+	private void insertImages () {
+		Image background=null;
+		Image startPoint=null;
+		Image targetPoint=null;
+		Image win=null;
+		Image curvedPipe=null;
+		Image straightPipe=null;
+		
+		try {
+			background = new Image(new FileInputStream(backgroundFileName.get()));
+			startPoint = new Image(new FileInputStream(startPointFileName.get()));
+			targetPoint = new Image(new FileInputStream(targetPointFileName.get()));
+			win = new Image(new FileInputStream(winFileName.get()));
+			curvedPipe = new Image(new FileInputStream(curvedPipeFileName.get()));
+			straightPipe = new Image(new FileInputStream(straightPipeFileName.get()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
